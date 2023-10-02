149f966620acd8d58b47a93b4c92ff6b75d41de3
AryanAD <aryanhohai@gmail.com>
2023-10-02 08:50:36 +0545

    job-portal

--- job-portal/src/App.jsx
+++ job-portal/src/App.jsx
@@ -1,7 +1,40 @@
 import "./App.css";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<BrowserRouter>
+				<ToastContainer
+					position="top-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="light"
+				/>
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
+					<Route
+						path="/login"
+						element={<Login />}
+					/>
+					<Route
+						path="/register"
+						element={<Register />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
