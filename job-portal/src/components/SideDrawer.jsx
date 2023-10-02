4ada42fb0ee09e78358ed8b9df7cf1332f8a682b
AryanAD <aryanhohai@gmail.com>
2023-10-02 16:40:27 +0545

    job-portal

--- job-portal/src/components/SideDrawer.jsx
+++ job-portal/src/components/SideDrawer.jsx
@@ -16,13 +16,12 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import TableChartRoundedIcon from "@mui/icons-material/TableChartRounded";
 import InsertPhotoRoundedIcon from "@mui/icons-material/InsertPhotoRounded";
 import React from "react";
 import DashBoard from "../layout/DashBoard";
 import { CTooltip } from "@coreui/react";
+import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
 
 const drawerWidth = 240;
 
@@ -151,35 +150,20 @@ const SideDrawer = () => {
 				</DrawerHeader>
 				<Divider />
 				<List>
-					{["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-						<ListItem
-							key={text}
-							disablePadding
-							sx={{ display: "block" }}>
-							<ListItemButton
-								sx={{
-									minHeight: 48,
-									justifyContent: open ? "initial" : "center",
-									px: 2.5,
-								}}>
-								<ListItemIcon
-									sx={{
-										minWidth: 0,
-										mr: open ? 3 : "auto",
-										justifyContent: "center",
-									}}>
-									{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+					<ListItem
+						disablePadding
+						sx={{ display: "block" }}>
+						<ListItemButton>
+							<CTooltip
+								content="Vendors"
+								placement="right">
+								<ListItemIcon>
+									<PeopleAltRoundedIcon />
 								</ListItemIcon>
-								<ListItemText
-									primary={text}
-									sx={{ opacity: open ? 1 : 0 }}
-								/>
-							</ListItemButton>
-						</ListItem>
-					))}
-				</List>
-				<Divider />
-				<List>
+							</CTooltip>
+							<ListItemText primary="Categories" />
+						</ListItemButton>
+					</ListItem>
 					<ListItem
 						disablePadding
 						sx={{ display: "block" }}>
