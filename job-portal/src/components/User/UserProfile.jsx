c1177ce8a7dfa5f27725ba9aee21ef7eae5918b1
AryanAD <aryanhohai@gmail.com>
2023-10-12 12:34:53 +0545

    job-portal

--- job-portal/src/components/User/UserProfile.jsx
+++ job-portal/src/components/User/UserProfile.jsx
@@ -1,2 +1,43 @@
-const UserProfile = () => {};
+import { Box } from "@mui/material";
+
+const UserProfile = () => {
+	return (
+		<>
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					width: "100vw",
+					height: "100vh",
+					bgcolor: "whitesmoke",
+				}}>
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						width: "80vw",
+						height: "100vh",
+						bgcolor: "white",
+					}}>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "space-between",
+							alignItems: "space-between",
+							bgcolor: "navyblue",
+							width: "20vw",
+							height: "100vh",
+						}}>
+						<Box></Box>
+						<Box></Box>
+						<Box></Box>
+					</Box>
+				</Box>
+			</Box>
+			;
+		</>
+	);
+};
 export default UserProfile;
