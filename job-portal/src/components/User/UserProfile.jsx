9c12c8c2abf663c65d085abc7cd9e2b8f5a0ff50
AryanAD <aryanhohai@gmail.com>
2023-10-13 13:36:24 +0545

    job-portal

--- job-portal/src/components/User/UserProfile.jsx
+++ job-portal/src/components/User/UserProfile.jsx
@@ -14,6 +14,7 @@ const UserProfile = () => {
 				}}>
 				<Box
 					sx={{
+						mt: 8,
 						display: "flex",
 						justifyContent: "center",
 						alignItems: "center",
@@ -24,21 +25,50 @@ const UserProfile = () => {
 					<Box
 						sx={{
 							display: "flex",
-							justifyContent: "space-between",
-							alignItems: "space-between",
+							flexDirection: "column",
+							alignItems: "center",
 							bgcolor: "blue",
 							width: "20vw",
 							height: "100vh",
+							gap: 4,
+							padding: 4,
 						}}>
-						<h1>hello</h1>
-						<Box></Box>
-						<Box></Box>
+						<Box
+							sx={{
+								display: "flex",
+								justifyContent: "center",
+								borderRadius: "11px",
+								bgcolor: "orange",
+								width: "18vw",
+								height: "36vh",
+							}}></Box>
+						<Box
+							sx={{
+								display: "flex",
+								borderRadius: "11px",
+								bgcolor: "purple",
+								width: "18vw",
+								height: "56vh",
+							}}></Box>
 					</Box>
 					<Box
 						sx={{
 							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							width: "60vw",
+							height: "100vh",
+							bgcolor: "yellowgreen",
 						}}>
-						<Box></Box>
+						<Box
+							sx={{
+								display: "flex",
+								margin: 5,
+								borderRadius: "11px",
+								width: "58vw",
+								height: "100vh",
+								bgcolor: "orangered",
+							}}></Box>
 					</Box>
 				</Box>
 			</Box>
