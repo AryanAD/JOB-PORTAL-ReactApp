3291e4648b75d22e4c5e58033157062f484a54a3
AryanAD <aryanhohai@gmail.com>
2023-10-15 16:08:40 +0545

    job-portal

--- job-portal/src/App.jsx
+++ job-portal/src/App.jsx
@@ -5,42 +5,42 @@ import { ToastContainer } from "react-toastify";
 // import SignupPage from "./components/SignupPage";
 import Modal from "./components/Modal";
 // import VendorLayout from "./layout/VendorLayout";
-// import AdminBanners from "./components/Admin/AdminBanners";
-// import AdminLayout from "./layout/AdminLayout";
-// import AdminCategories from "./components/Admin/AdminCategories";
-// import AdminVendors from "./components/Admin/AdminVendors";
+import AdminBanners from "./components/Admin/AdminBanners";
+import AdminLayout from "./layout/AdminLayout";
+import AdminCategories from "./components/Admin/AdminCategories";
+import AdminVendors from "./components/Admin/AdminVendors";
 import VendorJobModal from "./components/Vendor/VendorJobModal";
 import ViewerLayout from "./layout/ViewerLayout";
 // import ViewerHomePage from "./components/Viewer/ViewerHomePage";
 // import ViewerNavBar from "./components/Viewer/ViewerNavBar";
-import UserLayout from "./layout/UserLayout";
-import UserNavBar from "./components/User/UserNavBar";
-import UserHomePage from "./components/User/UserHomePage";
-import UserProfile from "./components/User/UserProfile";
-import UserJobs from "./components/User/UserJobs";
-import UserJobsModal from "./components/User/UserJobsModal";
-import UserJobsTable from "./components/User/UserJobsTable";
-import UserNewVendor from "./components/User/UserNewVendor";
+// import UserLayout from "./layout/UserLayout";
+// import UserNavBar from "./components/User/UserNavBar";
+// import UserHomePage from "./components/User/UserHomePage";
+// import UserProfile from "./components/User/UserProfile";
+// import UserJobs from "./components/User/UserJobs";
+// import UserJobsModal from "./components/User/UserJobsModal";
+// import UserJobsTable from "./components/User/UserJobsTable";
+// import UserNewVendor from "./components/User/UserNewVendor";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<ToastContainer
-					position="top-right"
-					autoClose={5000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-					theme="light"
-				/>
-				<ViewerLayout />
-				<Routes>
-					{/* Viewer Routes
+  return (
+    <>
+      <BrowserRouter>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <ViewerLayout />
+        <Routes>
+          {/* Viewer Routes
 					<Route element={<ViewerLayout />}>
 						<Route element={<ViewerNavBar />}>
 							<Route
@@ -57,67 +57,32 @@ function App() {
 							/>
 						</Route>
 					</Route> */}
-					{/* User Routes */}
-					<Route element={<UserLayout />}>
-						<Route element={<UserNavBar />}>
-							<Route
-								index
-								element={<UserHomePage />}
-							/>
-							<Route
-								path="user-profile"
-								element={<UserProfile />}
-							/>
-							<Route
-								path="/user-jobs"
-								element={<UserJobs />}
-							/>
+          {/* User Routes */}
+          {/* <Route element={<UserLayout />}>
+            <Route element={<UserNavBar />}>
+              <Route index element={<UserHomePage />} />
+              <Route path="user-profile" element={<UserProfile />} />
+              <Route path="/user-jobs" element={<UserJobs />} />
 
-							<Route
-								path="/user-jobs/*"
-								element={<UserJobsTable />}
-							/>
+              <Route path="/user-jobs/*" element={<UserJobsTable />} />
 
-							<Route
-								path="/user-jobs-modal"
-								element={<UserJobsModal />}
-							/>
-							<Route
-								path="/register-vendor"
-								element={<UserNewVendor />}
-							/>
-						</Route>
-					</Route>
-					{/* Admin Routes 
-					 <Route
-						path="/"
-						element={<AdminLayout />}>
-						<Route
-							path="/vendors"
-							element={<AdminVendors />}
-						/>
-						<Route
-							path="/categories"
-							element={<AdminCategories />}
-						/>
-						<Route
-							path="/banners"
-							element={<AdminBanners />}
-						/>
-					</Route> */}
-					{/* Other Routes */}
-					<Route
-						path="/modal"
-						element={<Modal />}
-					/>
-					<Route
-						path="/vendorjobmodal"
-						element={<VendorJobModal />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+              <Route path="/user-jobs-modal" element={<UserJobsModal />} />
+              <Route path="/register-vendor" element={<UserNewVendor />} />
+            </Route>
+          </Route> */}
+          {/* Admin Routes  */}
+          <Route path="/" element={<AdminLayout />}>
+            <Route path="/vendors" element={<AdminVendors />} />
+            <Route path="/categories" element={<AdminCategories />} />
+            <Route path="/banners" element={<AdminBanners />} />
+          </Route>
+          {/* Other Routes */}
+          <Route path="/modal" element={<Modal />} />
+          <Route path="/vendorjobmodal" element={<VendorJobModal />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
