8670c9a4ddd3c17cb639c406f0bf6d34c6161581
AryanAD <aryanhohai@gmail.com>
2023-10-20 15:16:26 +0545

    frontend

--- frontend/src/components/User/UserNewVendor.jsx
+++ frontend/src/components/User/UserNewVendor.jsx
@@ -64,10 +64,13 @@ const UserNewVendor = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const role = localStorage.getItem("role");
     const token = localStorage.getItem("token");
-    console.log(token);
-    console.log(role);
+    const role = localStorage.getItem("role");
+
+    if (!token || !role) {
+      // Handle the case where token or role is missing
+      return;
+    }
 
     try {
       const response = await axios.post(
@@ -88,11 +91,8 @@ const UserNewVendor = () => {
       );
 
       console.log(response, "re");
-      console.log(role);
-      if (role) {
-        toast.success("Successfully applied for Vendor!");
-        nav("/user");
-      }
+      toast.success("Successfully applied for Vendor!");
+      nav("/user");
     } catch (error) {
       console.error("API request failed: ", error);
     }
@@ -100,197 +100,186 @@ const UserNewVendor = () => {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <>
-        <Grid container component="main" sx={{ height: "100vh" }}>
-          <CssBaseline />
-          <Grid
-            item
-            xs={false}
-            sm={4}
-            md={7}
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${Image})`,
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
             sx={{
-              backgroundImage: `url(${Image})`,
-              backgroundRepeat: "no-repeat",
-              backgroundColor: (t) =>
-                t.palette.mode === "light"
-                  ? t.palette.grey[50]
-                  : t.palette.grey[900],
-              backgroundSize: "cover",
-              backgroundPosition: "center",
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
-          />
-          <Grid
-            item
-            xs={12}
-            sm={8}
-            md={5}
-            component={Paper}
-            elevation={6}
-            square
           >
-            <Box
-              sx={{
-                my: 8,
-                mx: 4,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
+            <Avatar
+              variant="rounded"
+              sx={{ m: 1, bgcolor: "#9C27B0", width: 50, height: 50 }}
             >
-              <Avatar
-                variant="rounded"
-                sx={{ m: 1, bgcolor: "#9C27B0", width: 50, height: 50 }}
+              <LockPersonRounded />
+            </Avatar>
+            <Typography component="h1" variant="h4">
+              Apply for Vendor
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PersonRounded />
+                    </InputAdornment>
+                  ),
+                }}
+                margin="normal"
+                fullWidth
+                id="name"
+                label="Full Name"
+                name="name"
+                autoFocus
+                type="text"
+                autoComplete="off"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MailRounded />
+                    </InputAdornment>
+                  ),
+                }}
+                margin="normal"
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoFocus
+                type="email"
+                autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <HomeRepairServiceRounded />
+                    </InputAdornment>
+                  ),
+                }}
+                margin="normal"
+                fullWidth
+                id="name"
+                label="Designation"
+                name="name"
+                autoFocus
+                type="text"
+                autoComplete="off"
+                onChange={(e) => setDesignation(e.target.value)}
+              />
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SettingsSuggestRounded />
+                    </InputAdornment>
+                  ),
+                }}
+                margin="normal"
+                fullWidth
+                id="name"
+                label="Service"
+                name="name"
+                autoFocus
+                type="text"
+                autoComplete="off"
+                onChange={(e) => setService(e.target.value)}
+              />
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PhoneRounded />
+                    </InputAdornment>
+                  ),
+                }}
+                margin="normal"
+                fullWidth
+                id="number"
+                label="Contact"
+                name="number"
+                autoFocus
+                type="number"
+                autoComplete="off"
+                onChange={(e) => setContact(e.target.value)}
+              />
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PushPinRounded />
+                    </InputAdornment>
+                  ),
+                }}
+                margin="normal"
+                fullWidth
+                id="name"
+                label="Address"
+                name="name"
+                autoFocus
+                type="text"
+                autoComplete="off"
+                onChange={(e) => setAddress(e.target.value)}
+              />
+              <Button
+                color="secondary"
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
               >
-                <LockPersonRounded />
-              </Avatar>
-              <Typography component="h1" variant="h4">
                 Apply for Vendor
-              </Typography>
-              <Box
-                onSubmit={handleSubmit}
-                component="form"
-                noValidate
-                sx={{ mt: 1 }}
-              >
-                <TextField
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <PersonRounded />
-                      </InputAdornment>
-                    ),
-                  }}
-                  margin="normal"
-                  fullWidth
-                  id="name"
-                  label="Full Name"
-                  name="name"
-                  autoFocus
-                  type="text"
-                  autoComplete="off"
-                  onChange={(e) => setName(e.target.value)}
-                />
-                <TextField
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <MailRounded />
-                      </InputAdornment>
-                    ),
-                  }}
-                  margin="normal"
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoFocus
-                  type="email"
-                  autoComplete="off"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <HomeRepairServiceRounded />
-                      </InputAdornment>
-                    ),
-                  }}
-                  margin="normal"
-                  fullWidth
-                  id="name"
-                  label="Designation"
-                  name="name"
-                  autoFocus
-                  type="text"
-                  autoComplete="off"
-                  onChange={(e) => setDesignation(e.target.value)}
-                />
-                <TextField
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <SettingsSuggestRounded />
-                      </InputAdornment>
-                    ),
-                  }}
-                  margin="normal"
-                  fullWidth
-                  id="name"
-                  label="Service"
-                  name="name"
-                  autoFocus
-                  type="text"
-                  autoComplete="off"
-                  onChange={(e) => setService(e.target.value)}
-                />
-                <TextField
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <PhoneRounded />
-                      </InputAdornment>
-                    ),
-                  }}
-                  margin="normal"
-                  fullWidth
-                  id="number"
-                  label="Contact"
-                  name="number"
-                  autoFocus
-                  type="number"
-                  autoComplete="off"
-                  onChange={(e) => setContact(e.target.value)}
-                />
-                <TextField
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <PushPinRounded />
-                      </InputAdornment>
-                    ),
-                  }}
-                  margin="normal"
-                  fullWidth
-                  id="name"
-                  label="Address"
-                  name="name"
-                  autoFocus
-                  type="text"
-                  autoComplete="off"
-                  onChange={(e) => setAddress(e.target.value)}
-                />
-                <Button
-                  color="secondary"
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Apply for Vendor
-                </Button>
-                <Grid container>
-                  <Grid item>
-                    <Link
-                      style={{
-                        textDecoration: "none",
-                        color: "indianred",
-                        fontWeight: "bold",
-                      }}
-                      to="/user"
-                    >
-                      {"↼ Back "}
-                    </Link>
-                  </Grid>
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link
+                    style={{
+                      textDecoration: "none",
+                      color: "indianred",
+                      fontWeight: "bold",
+                    }}
+                    to="/user"
+                  >
+                    {"↼ Back "}
+                  </Link>
                 </Grid>
-                <Copyright sx={{ mt: 5 }} />
-              </Box>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
             </Box>
-          </Grid>
+          </Box>
         </Grid>
-
-        <Footer />
-      </>
+      </Grid>
+      <Footer />
     </ThemeProvider>
   );
 };
