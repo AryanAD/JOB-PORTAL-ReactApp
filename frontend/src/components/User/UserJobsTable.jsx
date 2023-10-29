530eabd036f217073398708776ce46993db9ca49
AryanAD <aryanhohai@gmail.com>
2023-10-29 10:26:29 +0545

    frontend

--- frontend/src/components/User/UserJobsTable.jsx
+++ frontend/src/components/User/UserJobsTable.jsx
@@ -7,7 +7,6 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Button, styled } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import UserJobsModal from "./UserJobsModal";
 import { useState } from "react";
 
