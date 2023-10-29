bf0f75608ff3b080f775ea46b3976f8a68fa74a1
AryanAD <aryanhohai@gmail.com>
2023-10-29 09:22:01 +0545

    frontend

--- frontend/src/components/User/UserJobs.jsx
+++ frontend/src/components/User/UserJobs.jsx
@@ -1,19 +1,20 @@
 import { Divider } from "@mui/material";
-import UserJobsTable from "./UserJobsTable";
+// import UserJobsTable from "./UserJobsTable";
 import UserJobsSlides from "./UserJobsSlides";
+import UserJobsDisplay from "./UserJobsDisplay";
 
 const UserJobs = () => {
-	return (
-		<>
-			<UserJobsSlides />
-			<Divider
-				sx={{
-					paddingY: "20px",
-				}}
-			/>
-			<UserJobsTable />
-		</>
-	);
+  return (
+    <>
+      <UserJobsSlides />
+      <Divider
+        sx={{
+          paddingY: "20px",
+        }}
+      />
+      <UserJobsDisplay />
+    </>
+  );
 };
 
 export default UserJobs;
--- frontend/src/components/User/UserJobsDisplay.jsx
+++ frontend/src/components/User/UserJobsDisplay.jsx
@@ -10,10 +10,11 @@ import {
   Rating,
   Typography,
 } from "@mui/material";
-import StarRounded from "@mui/icons-material";
+import { StarRounded } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import Footer from "../../layout/Footer";
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8];
+const cards = [1, 2, 3, 4, 5, 6];
 
 const labels = {
   0.5: "Useless",
@@ -31,166 +32,159 @@ const labels = {
 const UserJobsDisplay = () => {
   const value = 4.5;
   return (
-    <div>
-      <Divider
+    <>
+      <Box
         sx={{
-          mt: 4,
-          mb: 8,
-        }}
-        variant="inset"
-        textAlign="left"
-      >
-        <Typography
-          sx={{
-            color: "black",
-            fontFamily: "nunito",
-            letterSpacing: "6px",
-            marginBottom: "5px",
-            fontWeight: "bold",
-            textAlign: "left",
-          }}
-          variant="h4"
-        >
-          Jobs Popular Today
-        </Typography>
-        <Divider variant="middle" />
-        <Divider variant="middle" />
-        <Divider variant="middle" />
-        <Divider variant="middle" />
-      </Divider>
-      <Grid
-        style={{
+          pb: 10,
           display: "flex",
-          justifyContent: "center",
+          flexDirection: "column",
           alignItems: "center",
-          maxWidth: "80vw",
-          margin: 5,
+          maxHeight: "100%",
         }}
-        container
-        spacing={4}
       >
-        {cards.map((card) => (
-          <Grid item key={card} xs={7} sm={7} md={4}>
-            <Card
-              sx={{
-                height: "100%",
-                maxWidth: "24vw",
-                display: "flex",
-                bgcolor: "#e9e9e9",
-                boxShadow: 24,
-                border: "1px solid #d8d8d8",
-                borderRadius: 3,
-              }}
-            >
-              <CardContent
+        <Divider
+          sx={{
+            mt: 4,
+            mb: 8,
+          }}
+          variant="inset"
+          textAlign="left"
+        >
+          <Typography
+            sx={{
+              color: "black",
+              fontFamily: "nunito",
+              letterSpacing: "6px",
+              marginBottom: "5px",
+              fontWeight: "bold",
+              textAlign: "left",
+            }}
+            variant="h4"
+          >
+            Jobs Popular Today
+          </Typography>
+          <Divider variant="middle" />
+          <Divider variant="middle" />
+          <Divider variant="middle" />
+          <Divider variant="middle" />
+        </Divider>
+        <Grid
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            maxWidth: "80vw",
+            margin: 5,
+          }}
+          container
+          spacing={4}
+        >
+          {cards.map((card) => (
+            <Grid item key={card} xs={7} sm={3} md={7}>
+              <Card
                 sx={{
-                  gap: 2,
-                  flexGrow: 1,
+                  height: "100%",
+                  maxWidth: "24vw",
                   display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
+                  bgcolor: "#e9e9e9",
+                  boxShadow: 24,
+                  border: "1px solid #d8d8d8",
+                  borderRadius: 3,
                 }}
               >
-                <Box>
-                  <CardMedia
-                    gutterBottom
-                    sx={{
-                      // 16:9
-                      width: "130px",
-                      height: "130px",
-                      border: "4px inset #277aff",
-                      borderRadius: "13px",
-                    }}
-                    image="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
-                  />
-                </Box>
-                <Box
+                <CardContent
                   sx={{
-                    height: "100%",
-                    width: "100%",
+                    gap: 2,
+                    flexGrow: 1,
                     display: "flex",
-                    flexDirection: "column",
-                    alignItems: "space-between",
+                    justifyContent: "space-between",
+                    alignItems: "center",
                   }}
                 >
-                  <Typography
-                    gutterBottom={false}
-                    variant="h4"
-                    sx={{
-                      mb: 0,
-                      pb: 0,
-                      fontWeight: "bolder",
-                      color: "#444",
-                      fontFamily: "monospace",
-                    }}
-                    component="h2"
-                  >
-                    Job Name
-                  </Typography>
                   <Box
                     sx={{
-                      width: 200,
+                      height: "100%",
+                      width: "100%",
                       display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
+                      flexDirection: "column",
+                      alignItems: "space-between",
                     }}
                   >
-                    <Rating
-                      name="text-feedback"
-                      value={value}
-                      readOnly
-                      precision={0.5}
-                      emptyIcon={
-                        <StarRounded
-                          style={{ opacity: 0.55 }}
-                          fontSize="inherit"
-                        />
-                      }
-                    />
-                    <Box sx={{ ml: 2 }}>{labels[value]}</Box>
-                  </Box>
-                  <CardActions>
-                    <Link
-                      style={{
-                        width: "100%",
+                    <Typography
+                      gutterBottom={false}
+                      variant="h4"
+                      sx={{
+                        mb: 0,
+                        pb: 0,
+                        fontWeight: "bolder",
+                        color: "#444",
+                        fontFamily: "monospace",
                       }}
-                      to="/user/jobs"
+                      component="h2"
                     >
-                      <Button
-                        sx={{
-                          my: 0.5,
-                          color: "black",
-                          "&:hover": { bgcolor: "#277aff", color: "white" },
+                      Software Developer
+                    </Typography>
+                    <Box sx={{ maxWidth: "60vw" }}>
+                      <Typography variant="body2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Soluta reiciendis expedita nostrum optio error culpa
+                        asperiores enim deleniti. Nobis reiciendis placeat enim
+                        quod repellat odit magnam eligendi architecto facilis
+                        similique.
+                      </Typography>
+                    </Box>
+                    <Box
+                      sx={{
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Rating
+                        name="text-feedback"
+                        value={value}
+                        readOnly
+                        precision={0.5}
+                        emptyIcon={
+                          <StarRounded
+                            style={{ opacity: 0.55 }}
+                            fontSize="inherit"
+                          />
+                        }
+                      />
+                      <Box sx={{ ml: 2 }}>{labels[value]}</Box>
+                    </Box>
+                    <CardActions>
+                      <Link
+                        style={{
+                          width: "100%",
                         }}
-                        fullWidth
-                        variant="outlined"
-                        size="medium"
+                        to="/user/jobs"
                       >
-                        More Info
-                      </Button>
-                    </Link>
-                  </CardActions>
-                </Box>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-      <Link to="/user/jobs">
-        <Button
-          sx={{
-            mt: 10,
-            scale: "2",
-            bgcolor: "#7ed957",
-            "&:hover": { bgcolor: "#00bf63" },
-          }}
-          size="small"
-          variant="contained"
-        >
-          More Jobs
-        </Button>
-      </Link>
-    </div>
+                        <Button
+                          sx={{
+                            my: 0.5,
+                            color: "black",
+                            "&:hover": { bgcolor: "#277aff", color: "white" },
+                          }}
+                          fullWidth
+                          variant="outlined"
+                          size="medium"
+                        >
+                          More Info
+                        </Button>
+                      </Link>
+                    </CardActions>
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+      <Footer />
+    </>
   );
 };
 
