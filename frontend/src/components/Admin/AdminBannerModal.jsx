860485fcf49003fd2e1e843b1fbdd965ff7922d6
AryanAD <aryanhohai@gmail.com>
2023-10-30 09:51:44 +0545

    frontend

---
+++ frontend/src/components/Admin/AdminBannerModal.jsx
@@ -0,0 +1,129 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import { useSpring, animated } from "@react-spring/web";
+import { useNavigate } from "react-router";
+import { TextField } from "@mui/material";
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const {
+    children,
+    in: open,
+    onClick,
+    onEnter,
+    onExited,
+    ownerState,
+    ...other
+  } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter(null, true);
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited(null, true);
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {React.cloneElement(children, { onClick })}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element.isRequired,
+  in: PropTypes.bool,
+  onClick: PropTypes.any,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+  ownerState: PropTypes.any,
+};
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: "11px",
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+};
+
+const AdminBannerModal = ({ modalOpen, modalClose }) => {
+  const nav = useNavigate();
+  return (
+    <Box>
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        open={modalOpen}
+        onClose={modalClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            TransitionComponent: Fade,
+          },
+        }}
+      >
+        <Fade in={modalOpen}>
+          <Box sx={style}>
+            <TextField
+              variant="outlined"
+              type="text"
+              label="Category Title"
+              color="info"
+            />
+            <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                mt: 2,
+                mx: "auto",
+                gap: 1,
+              }}
+            >
+              <label
+                style={{
+                  padding: 2,
+                  fontFamily: "monospace",
+                  color: "#555",
+                  borderBottom: "2px solid #333",
+                }}
+              >
+                Upload Image:
+              </label>
+              <input style={{ paddingTop: 1 }} type="file" accept="image/*" />
+            </Box>
+            <Button
+              color="success"
+              variant="outlined"
+              sx={{ mt: 2, "&:hover": { bgcolor: "#bfd7c0", color: "green" } }}
+              onClick={(nav("/admin/banners"), modalClose)}
+            >
+              Add
+            </Button>
+          </Box>
+        </Fade>
+      </Modal>
+    </Box>
+  );
+};
+
+export default AdminBannerModal;
--- frontend/src/components/Admin/AdminBanners.jsx
+++ frontend/src/components/Admin/AdminBanners.jsx
@@ -10,71 +10,104 @@ import {
 } from "@mui/material";
 import LaunchIcon from "@mui/icons-material/Launch";
 import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import { AddRounded } from "@mui/icons-material";
+import { useState } from "react";
+import AdminBannerModal from "./AdminBannerModal";
 
 const AdminBanners = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <Box>
-      <Divider variant="inset" textAlign="left">
-        <Typography
+    <>
+      <Box>
+        <Divider variant="inset" textAlign="left">
+          <Typography
+            sx={{
+              color: "black",
+              fontFamily: "nunito",
+              letterSpacing: "6px",
+              marginBottom: "5px",
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+            variant="h4"
+          >
+            Banners
+          </Typography>
+        </Divider>
+        <Card
           sx={{
-            color: "black",
-            fontFamily: "nunito",
-            letterSpacing: "6px",
-            marginBottom: "5px",
-            fontWeight: "bold",
-            textAlign: "center",
+            margin: "20px",
+            maxWidth: 345,
+            boxShadow: "0 10px 20px rgba(0, 0, 0, 0.2)",
+            borderRadius: "12px",
           }}
-          variant="h4"
         >
-          Banners
-        </Typography>
-      </Divider>
-      <Card
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Banner Title
+            </Typography>
+          </CardContent>
+          <CardActions
+            sx={{
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
+            <Button
+              startIcon={<LaunchIcon />}
+              color="info"
+              fullWidth
+              variant="contained"
+              size="small"
+            >
+              View
+            </Button>
+            <Button
+              startIcon={<DeleteRoundedIcon />}
+              color="error"
+              fullWidth
+              variant="contained"
+              size="small"
+            >
+              Delete
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+      <Box
         sx={{
-          margin: "20px",
-          maxWidth: 345,
-          boxShadow: "0 10px 20px rgba(0, 0, 0, 0.2)",
-          borderRadius: "12px",
+          width: "100%",
+          position: "absolute",
+          left: "45%",
+          right: "50%",
+          bottom: 30,
         }}
       >
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="140"
-          image="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Banner Title
-          </Typography>
-        </CardContent>
-        <CardActions
-          sx={{
-            display: "flex",
-            justifyContent: "space-around",
-          }}
+        <Button
+          variant="contained"
+          color="success"
+          startIcon={<AddRounded />}
+          onClick={handleOpenModal}
         >
-          <Button
-            startIcon={<LaunchIcon />}
-            color="info"
-            fullWidth
-            variant="contained"
-            size="small"
-          >
-            View
-          </Button>
-          <Button
-            startIcon={<DeleteRoundedIcon />}
-            color="error"
-            fullWidth
-            variant="contained"
-            size="small"
-          >
-            Delete
-          </Button>
-        </CardActions>
-      </Card>
-    </Box>
+          Add Banners
+        </Button>
+      </Box>
+      <AdminBannerModal modalOpen={isModalOpen} modalClose={handleCloseModal} />
+    </>
   );
 };
 
