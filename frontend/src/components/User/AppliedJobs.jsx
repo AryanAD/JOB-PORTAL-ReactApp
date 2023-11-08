facada3fe016bcbda14a3ab6cbf6256241f233d0
Aryan AD <aryanad@pop-os.localdomain>
2023-11-08 13:32:43 +0545

    frontend

--- frontend/src/components/User/AppliedJobs.jsx
+++ frontend/src/components/User/AppliedJobs.jsx
@@ -1,6 +1,7 @@
 import { Box, Card, CardContent, Typography } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
 import { apiText } from "../../global/API";
+import Chip from "@mui/material-next/Chip";
 
 const AppliedJobs = () => {
   const [jobsData, setJobsData] = useState([]);
@@ -20,15 +21,56 @@ const AppliedJobs = () => {
   }, [fetchJobsData]);
   return (
     <>
-      <Box>
-        <Box
-          variant="outlined"
-          sx={{ maxWidth: "100%", bgcolor: "yellowgreen" }}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+          overflow: "hidden",
+        }}
+      >
+        <Card
+          elevation={false}
+          sx={{
+            width: "100%",
+            borderLeft: "6px solid #999",
+            "&:hover": {
+              borderColor: "orangered",
+            },
+          }}
         >
           <CardContent>
-            <Typography>aaa{jobsData[0]?.jobId.title}</Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              sx={{ fontFamily: "monospace" }}
+            >
+              {jobsData[0]?.jobId.title}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ fontFamily: "monospace", color: "gray", mb: 2 }}
+            >
+              {jobsData[0]?.jobId.title}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2,
+                width: "90%",
+                justifyContent: "space-around",
+              }}
+            >
+              <Chip />
+              <Chip />
+              <Chip />
+              <Chip />
+            </Box>
           </CardContent>
-        </Box>
+        </Card>
       </Box>
     </>
   );
